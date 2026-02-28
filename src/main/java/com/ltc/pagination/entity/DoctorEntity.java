1fbacbb3d0a2162bf375f4a7e0fdb786e1729c62
Ali <aaliyev14291@ada.edu.az>
2026-02-28 05:24:42 +0400

    DoctorEntity

--- src/main/java/com/ltc/pagination/entity/DoctorEntity.java
+++ src/main/java/com/ltc/pagination/entity/DoctorEntity.java
@@ -1,4 +1,28 @@
 package com.ltc.pagination.entity;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "doctors")
 public class DoctorEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String fullName;
+    private String specialization;
+    private boolean available;
+
+    @OneToMany(mappedBy = "doctor", cascade = CascadeType.ALL)
+    private List<AppointmentEntity> appointments;
+
 }
