498467db6b5756e7e0f045c040825d6311786dc8
Ali <aaliyev14291@ada.edu.az>
2026-03-02 08:01:58 +0400

    exceptions

---
+++ src/main/java/com/ltc/pagination/exception/DoctorHasAppointmentException.java
@@ -0,0 +1,8 @@
+package com.ltc.pagination.exception;
+
+public class DoctorHasAppointmentException extends RuntimeException {
+
+    public DoctorHasAppointmentException(String message) {
+        super(message);
+    }
+}
